414554bed657619956dfbf680fb5da9e6747b4a3
vumg <ab17100222@gmail.com>
2019-10-30 11:03:09 -0600

    arregle el codigo de la practica de javascript

--- src/8/js/DOM.js
+++ src/8/js/DOM.js
@@ -1,13 +1,13 @@
 console.log(window)
 
 window.onload = () => {
-    console.log("hola"),
+    contadorMain();
+}
+
+function contadorMain(){
     document.getElementById("#h2").innerHTML = "Contador";
     boton = document.getElementById("button");
     botonReset = document.getElementById("button2");
-
     boton.onclick = () => boton.value++;
     botonReset.onclick = () => boton.value = 0;
-
 }
-
